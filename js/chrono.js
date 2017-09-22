be83ded1c72ffe247d8d7f35d96625ab552a9e3a
dcaron1 <dylan.caron81@laposte.net>
2017-09-22 14:23:10 +0200

    contructeur + run
    - ajout du constructeur
    - ajout de la méthode run()
      => erreur : affichage "undefined"

---
+++ js/chrono.js
@@ -0,0 +1,68 @@
+/***************************************************************
+* Fichier : Chrono.js (object declaration)
+* Projet  : chrono_web
+* 
+* arborescence :
+* js
+* |- main.js
+* |- class
+* |  |- Chrono.js *
+* 
+* Dev.    : Dylan CARON
+* Update  : 22/09/2017
+*   => Création de la classe Chrono
+***************************************************************/
+"use strict"
+
+class Chrono {
+  /*
+  * Objet Chrono
+  *
+  * CONSTRUCTEUR
+  * Chrono() : Chrono object
+  *
+  * ATTRIBUTS
+  *
+  * METHODES
+  *
+  */
+
+  constructor() {
+  //==============================
+  //========  ATTRIBUTS  =========
+  //==============================
+    this.hours = 0;
+    this.mins = 0;
+    this.secs = 0;
+    this.cs = 0;
+
+  }
+
+//==============================
+//=========  METHODES  =========
+//==============================
+
+  run() {
+    setInterval(function() {
+      if(this.mins > 59) {
+        this.mins = 0;
+        this.hours ++;
+      } else if(this.secs > 59){
+        this.secs = 0;
+        this.mins ++;
+      } else if(this.cs > 99) {
+        this.cs = 0;
+        this.secs ++;
+      } else {
+        this.cs ++;
+      }
+
+      console.log(
+        this.hours + ':' +
+        this.mins + ':' + 
+        this.secs + ':' + 
+        this.cs
+      );
+    }, 10)
+  }
+}
